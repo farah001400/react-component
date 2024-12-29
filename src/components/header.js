e4f12c6b090e6e8057c3a81c1b3acf747b42dd09
farah001400 <farah001400@gmail.com>
2024-12-29 23:21:22 +0500

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+function Header(){
+    return(
+        <ul className="list">
+            <li>home</li>
+            <li>about</li>
+            <li>service</li>
+            <li>contact us</li>
+            <li>gallery</li>
+        </ul>
+    )
+}
+export {Header};
